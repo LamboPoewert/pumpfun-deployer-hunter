797abac2bb576700ffb2376289a66ea04d6c9df9
LamboPoewert <91011698+LamboPoewert@users.noreply.github.com>
2026-01-18 12:33:47 +0100

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -197,13 +197,13 @@ export default function Home() {
                       </div>
                     </div>
 
-                    {/* Token Address Info */}
+                    {/* Dev Wallet Info */}
                     <div className="mt-4 pt-4 border-t border-cyan-900">
                       <div className="font-rajdhani text-xs text-gray-400 mb-1">
-                        TOKEN ADDRESS
+                        DEV WALLET
                       </div>
                       <div className="font-mono text-sm text-gray-300 break-all">
-                        {token.mint}
+                        {token.deployer}
                       </div>
                     </div>
                   </div>
